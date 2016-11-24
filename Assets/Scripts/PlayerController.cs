d746423ede4454e4402256ccaae691da80b25d3f
LeandroDotta <leandro.dotta@yahoo.com.br>
2016-11-24 17:00:27 -0200

    Implementando o rolamento (dodge) do personagem
    Ainda não está finalizado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
     public float speed;
     public float jumpForce;
+    public float rollSpeed;
     public LayerMask groundLayer;
     public Transform groundCheck;
 
+    // Inputs
     private bool jump;
-    private bool grounded;
+    private bool roll;
     private float axisHorizontal;
 
+    // States
+    private bool grounded;
+    private bool rolling;
+
+    // Components
     private Animator anim;
     private Rigidbody2D rb2d;
     private SpriteRenderer spriteRenderer;
@@ -28,6 +35,7 @@ public class PlayerController : MonoBehaviour {
         // Verifica os botões pressionado pelo jogador
         axisHorizontal = Input.GetAxis("Horizontal");
         jump = Input.GetButtonDown("Jump");
+        roll = Input.GetKeyDown(KeyCode.S);
 
         // Vira o personagem de acordo com a direção que ele está indo
         if (axisHorizontal > 0)
@@ -41,9 +49,21 @@ public class PlayerController : MonoBehaviour {
 
         // Verifica se está tocando o chão (para evitar pulos enquanto estiver no ar)
         grounded = Physics2D.CircleCast(groundCheck.position, 0.05f, Vector2.zero, 0, groundLayer);
+        rolling = anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerRoll");
 
         // Atualiza a animação com caso ele esteja movimentando
-        anim.SetFloat("axisHorizontal", Mathf.Abs(axisHorizontal));
+        anim.SetFloat("axisHorizontal", Mathf.Abs(rb2d.velocity.x));
+        anim.SetBool("grounded", grounded);
+
+        if (rolling)
+        {
+            
+            axisHorizontal += rollSpeed * Mathf.Sign(axisHorizontal);
+        }
+        else if (grounded && roll)
+        {
+            anim.SetTrigger("rolling");
+        }
     }
 
     void FixedUpdate()
@@ -53,7 +73,7 @@ public class PlayerController : MonoBehaviour {
         rb2d.velocity = movement;
 
         // Aplica a física de pulo
-        if (jump && grounded)
+        if (jump && grounded && !rolling)
         {
             rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
