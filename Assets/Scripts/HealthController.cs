0da70ff665a26431daeaeb296b94b37d03086569
LeandroDotta <leandro.dotta@yahoo.com.br>
2016-11-28 19:23:47 -0200

    Esquiva, Animações e Inputs
    Foram alteradas as animações do personagem de acordo com o spritesheet
    de tests disponibilizado no Trello;
    
    Foram definidos os botões de ações disponíveis (em Input Manager)
    
    Foi implementada a esquiva do jogador onde ele realiza o movimento se
    abaixando para esquivar de obstaculos (ainda é preciso alterar a colisão
    do personagem ao se abaixar)

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthController : MonoBehaviour {
+
+    /// <summary>
+    /// Saúde atual do objeto
+    /// </summary>
+    public float health;
+    /// <summary>
+    /// Saúde inicial do objeto
+    /// </summary>
+    public float startingHealth;
+    /// <summary>
+    /// Saúde máxima do objeto
+    /// </summary>
+    public float maxHealth = 100;
+
+    /// <summary>
+    /// Tempo que o objeto fica invulnerável após receber dano (em segundos).
+    /// </summary>
+    public float invulnerableTime;
+    /// <summary>
+    /// Contador de tempo de invulnerabilidade
+    /// </summary>
+    private float invulnerableCounter;
+
+    /// <summary>
+    /// Indica se o objeto está invulnerável ou não.
+    /// </summary>
+    [HideInInspector]
+    public bool isInvulnerable;
+
+    // Eventos
+    public delegate void EnterInvulnerableModeAction();
+    public event EnterInvulnerableModeAction OnEnterInvulnerableMode;
+    public delegate void ExitInvulnerableModeAction();
+    public event ExitInvulnerableModeAction OnExitInvulnerableMode;
+    public delegate void TakeDamageAction(float amount);
+    public event TakeDamageAction OnTakeDamage;
+    public delegate void ReachZeroHealthAction();
+    public event ReachZeroHealthAction OnReachZeroHealth;
+
+    // Use this for initialization
+    void Start () {
+        // Atribui o valor inicial para a saúde do objeto.
+        if (startingHealth <= 0)
+            health = 1;
+        else if (startingHealth > maxHealth)
+            health = maxHealth;
+        else
+            health = startingHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isInvulnerable)
+        {
+            // Conta o tempo em que está invulnerável
+            invulnerableCounter += Time.deltaTime;
+
+            // Ao atingir o tempo, sai do modo de invulnerabildade
+            if (invulnerableCounter >= invulnerableTime)
+            {
+                invulnerableCounter = 0;
+                isInvulnerable = false;
+
+                // Chama o evento de sair do modo invulnerável
+                OnExitInvulnerableMode();
+            }
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (isInvulnerable) // Se estiver no modo invulnerável, não recebe dano.
+            return;
+
+        // Remove a quantidade de saúde passada como parâmetro
+        health -= amount;
+
+        // Chama o evento ao receber dano
+        OnTakeDamage(amount);
+
+        // Chama evento ao zerar a saúde do objeto
+        if (health <= 0)
+        {
+            OnReachZeroHealth();
+        }
+
+        // Altera o estado para invulnerável caso seja necessário
+        if (invulnerableTime > 0)
+        {
+            isInvulnerable = true;
+
+            // Chama o evento de entrar em modo invulnerável
+            OnEnterInvulnerableMode();
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,29 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
     public float speed;
     public float jumpForce;
-    public float rollSpeed;
+    public float rollTime;
+    public float rollDistance;
     public LayerMask groundLayer;
     public Transform groundCheck;
 
     // Inputs
-    private bool jump;
-    private bool roll;
+    private bool pressAttack;
+    private bool pressGrab;
+    private bool pressRoll;
+    private bool pressJump;
     private float axisHorizontal;
 
     // States
     private bool grounded;
     private bool rolling;
 
+    private bool inputLocked;
+
     // Components
     private Animator anim;
     private Rigidbody2D rb2d;
     private SpriteRenderer spriteRenderer;
     
-
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -30,12 +34,45 @@ public class PlayerController : MonoBehaviour {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    void FixedUpdate()
+    {
+        // Movimenta o personagem
+        Vector2 movement = new Vector2(axisHorizontal * speed, rb2d.velocity.y);
+        rb2d.velocity = movement;
+
+        // Aplica a física de pulo
+        if (grounded && !rolling)
+        {
+            if (pressJump)
+            {
+                Jump();
+            }
+            else if (pressRoll)
+            {
+                StartCoroutine("Roll");
+            }
+        }       
+    }
+
     void Update()
     {
         // Verifica os botões pressionado pelo jogador
-        axisHorizontal = Input.GetAxis("Horizontal");
-        jump = Input.GetButtonDown("Jump");
-        roll = Input.GetKeyDown(KeyCode.S);
+        if (!inputLocked)
+        {
+            axisHorizontal = Input.GetAxis("Horizontal");
+            pressAttack = Input.GetButtonDown("Attack");
+            pressGrab = Input.GetButtonDown("Grab");
+            pressRoll = Input.GetButtonDown("Roll");
+            pressJump = Input.GetButtonDown("Jump");
+        }
+        else
+        {
+            axisHorizontal = 0;
+            pressAttack = false;
+            pressGrab = false;
+            pressRoll = false;
+            pressJump = false;
+        }
 
         // Vira o personagem de acordo com a direção que ele está indo
         if (axisHorizontal > 0)
@@ -48,40 +85,53 @@ public class PlayerController : MonoBehaviour {
         }
 
         // Verifica se está tocando o chão (para evitar pulos enquanto estiver no ar)
-        grounded = Physics2D.CircleCast(groundCheck.position, 0.05f, Vector2.zero, 0, groundLayer);
-        rolling = anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerRoll");
+        grounded = Physics2D.CircleCast(groundCheck.position, 0.05f, Vector2.zero, 0, groundLayer) && rb2d.velocity.y == 0;
 
         // Atualiza a animação com caso ele esteja movimentando
         anim.SetFloat("axisHorizontal", Mathf.Abs(rb2d.velocity.x));
         anim.SetBool("grounded", grounded);
-
-        if (rolling)
-        {
-            
-            axisHorizontal += rollSpeed * Mathf.Sign(axisHorizontal);
-        }
-        else if (grounded && roll)
-        {
-            anim.SetTrigger("rolling");
-        }
-    }
-
-    void FixedUpdate()
-    {
-        // Movimenta o personagem
-        Vector2 movement = new Vector2(axisHorizontal * speed, rb2d.velocity.y);
-        rb2d.velocity = movement;
-
-        // Aplica a física de pulo
-        if (jump && grounded && !rolling)
-        {
-            rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-        }
     }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, 0.05f);
+    }
+
+    private void Jump()
+    {
+        rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+        anim.SetTrigger("jump");
+    }
+
+    IEnumerator Roll()
+    {
+        rolling = true;
+        inputLocked = true;
+
+        Vector2 startPos = rb2d.position;
+        Vector2 endPos = rb2d.position;
+        endPos.x = spriteRenderer.flipX ? rb2d.position.x - rollDistance : rb2d.position.x + rollDistance;
+
+        float currentTime = 0;
+
+        anim.SetTrigger("roll");
+        while (true)
+        {
+            currentTime += Time.fixedDeltaTime;
+            float t = currentTime / rollTime;
+
+            Vector2 currentPos = Vector2.Lerp(startPos, endPos, t);
+            rb2d.MovePosition(currentPos);
+
+            if (t >= 1f)
+                break;
+
+            yield return null;
+        }
+
+        rolling = false;
+        inputLocked = false;
     }
 }
