2d0663a6572b36140040c02e9302f2ee0321f29b
DragonSkyX <dragonskyx@gmail.com>
2016-12-14 17:09:10 -0200

    Adição da STAMINA E VIDA
    Adição do Script de Stamina e Vida.

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour 
+   
+{
+    public float valor;
+  
+    
+
+    public void OnDestroy()
+    {
+            
+        Destroy(this.gameObject);
+
+}
+
+    [CustomEditor(typeof(Pickup))]
+    public class PickupEditor : Editor
+    {
+        private Pickup objeto;
+        private List<string> tags;
+
+        void OnEnable()
+        {
+            objeto = (Pickup)target;
+        }
+
+
+    }
+    }
+    
+
+
+
+
+    // Use this for initialization
+
+
+// Update is called once per frame
+
+
---
+++ Assets/Scripts/Stamina.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+
+public class Stamina : Pickup
+{
+
+
+    void OnTriggerEnter2D(Collider2D Player)
+
+
+    {
+
+
+
+        EnergyController energy = Player.GetComponent<EnergyController>();
+
+        energy.Add(valor);
+
+        base.OnDestroy();
+    }
+
+
+
+}
