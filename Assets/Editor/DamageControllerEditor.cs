3cc11218dafcd6e0541fbd39b06a10b2784f0519
LeandroDotta <leandro.dotta@yahoo.com.br>
2017-01-27 15:29:48 -0200

    Ajustes
    O script para o editor de DamageController foi movido para a pasta
    Editor, para que não seja usado quando for criada uma build

---
+++ Assets/Editor/DamageControllerEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DamageController))]
+public class DamageControllerEditor : Editor
+{
+    private DamageController objeto;
+    private List<string> tags;
+
+    void OnEnable()
+    {
+        objeto = (DamageController)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector(); // desenha o insector padrão definido no MonoBehaviour
+
+        // Cria um campo para selecionar uma ou mais tags
+        objeto.targetTagsMask = EditorGUILayout.MaskField("Target Tags", objeto.targetTagsMask, UnityEditorInternal.InternalEditorUtility.tags);
+        tags = new List<string>();
+
+        // Quando algum campo for alterado
+        if (GUI.changed)
+        {
+            // Adiciona as tags selecionadas a uma lista e no final atribui o valor ao script desejado
+            for (int i = 0; i < UnityEditorInternal.InternalEditorUtility.tags.Length; ++i)
+            {
+                int layer = 1 << i;
+
+                if ((objeto.targetTagsMask & layer) != 0) // a tag na posição i foi selecionada
+                {
+                    tags.Add(UnityEditorInternal.InternalEditorUtility.tags[i]);
+                }
+            }
+
+            objeto.targetTags = tags.ToArray();
+        }
+    }
+}
--- Assets/Scripts/DamageController.cs
+++ Assets/Scripts/DamageController.cs
@@ -1,7 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
-using System.Collections.Generic;
 using System.Linq;
 
 public class DamageController : MonoBehaviour {
@@ -32,43 +29,6 @@ public class DamageController : MonoBehaviour {
             }
 
             OnHitTarget();
-        }
-    }
-}
-
-[CustomEditor(typeof(DamageController))]
-public class DamageControllerEditor : Editor {
-    private DamageController objeto;
-    private List<string> tags;
-
-    void OnEnable()
-    {
-        objeto = (DamageController)target;
-    }
-
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector(); // desenha o insector padrão definido no MonoBehaviour
-
-        // Cria um campo para selecionar uma ou mais tags
-        objeto.targetTagsMask = EditorGUILayout.MaskField("Target Tags", objeto.targetTagsMask, UnityEditorInternal.InternalEditorUtility.tags);
-        tags = new List<string>();
-
-        // Quando algum campo for alterado
-        if (GUI.changed)
-        {
-            // Adiciona as tags selecionadas a uma lista e no final atribui o valor ao script desejado
-            for (int i = 0; i < UnityEditorInternal.InternalEditorUtility.tags.Length; ++i)
-            {
-                int layer = 1 << i;
-
-                if((objeto.targetTagsMask & layer) != 0) // a tag na posição i foi selecionada
-                {
-                    tags.Add(UnityEditorInternal.InternalEditorUtility.tags[i]);
-                }
-            }
-
-            objeto.targetTags = tags.ToArray();
         }
     }
 }
